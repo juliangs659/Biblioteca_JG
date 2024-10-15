d7ec694e7cc33bcb689956dfe6dadb59e2940a62
Julian <julian_agutierrezs@soy.sena.edu.co>
2024-10-14 19:23:50 -0500

    Agregar servicios a Ususarios

--- src/main/java/com/example/Biblioteca_Digital_JG/Biblioteca_JG/Services/UsuarioServicio.java
+++ src/main/java/com/example/Biblioteca_Digital_JG/Biblioteca_JG/Services/UsuarioServicio.java
@@ -5,6 +5,7 @@ import com.example.Biblioteca_Digital_JG.Biblioteca_JG.Repositories.UsuarioRepos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +24,33 @@ public class UsuarioServicio {
   private Optional<Usuarios> getUsuarioById(int idUsuario){
     return usuarioRepositorio.findById(idUsuario);
   }
+
+  //Obtener todos los usuarios
+  public List<Usuarios> getAllUsuarios() {
+    return usuarioRepositorio.findAll();
+  }
+
+  //Eliminar usuario por ID
+  public void deleteUsuarioById(int idUsuario) {
+    usuarioRepositorio.deleteById(idUsuario);
+  }
+
+  //Actuqlizar Usuario
+  public Usuarios updateUsuario(int idUsuario, Usuarios usuarioActualizado) {
+    Optional<Usuarios> usuarioExistente = usuarioRepositorio.findById(idUsuario);
+
+    if (usuarioExistente.isPresent()) {
+      Usuarios usuario = usuarioExistente.get();
+      usuario.setNombre(usuarioActualizado.getNombre());
+      usuario.setEmail(usuarioActualizado.getEmail());
+      // Agregar otras actualizaciones de campos según sea necesario
+      return usuarioRepositorio.save(usuario);
+    } else {
+      return null; // O lanzar una excepción si el usuario no existe
+    }
+  }
+
+
 
 
 }
